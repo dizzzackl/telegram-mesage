4305991b23ea57f16dd77687e59d59a3ad59dd51
dizzzackl <154916956+dizzzackl@users.noreply.github.com>
2024-03-05 18:48:07 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import telebot
+app = Flask(__name__)
+bot = telebot.TeleBot('7090253365:AAGbr_LloonFPGCIPbOgSRFfTAwt5dsmcMs')
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/enter_message', methods=['POST'])
+def enter_message():
+    text = request.form['text']
+    chat_id = request.form['chat_id']
+    bot.send_message(chat_id=chat_id, text=text)
+    return render_template("gotovo.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
